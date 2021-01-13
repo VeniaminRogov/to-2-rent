ef40db634d0d84765b63d4bc8dbdb4d89999c9a5
Rogov <rogov.veniamin@gmail.com>
2021-01-13 15:49:35 +0700

    без media

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".slider").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="../img/next.svg" alt="next arrow"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../img/prev.svg" alt="prev arrow"></button>',
+  });
+});
